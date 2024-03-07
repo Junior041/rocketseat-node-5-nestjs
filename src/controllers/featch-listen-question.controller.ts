034f2ffcfeb79331583d00a3e117e7a5ab8d8a23
Junior <ismaeljunior1508@gmail.com>
2024-03-07 13:47:55 -0300

    test-e2e

--- src/controllers/featch-listen-question.controller.ts
+++ src/controllers/featch-listen-question.controller.ts
@@ -24,7 +24,7 @@ export class FetchListenQuestionController {
 	async handle(
 		@Query("page", queryValidationPipe) page: PageQueryParamSchema
 	) {
-		const perPage = 1;
+		const perPage = 20;
 
 		const questions = await this.prisma.question.findMany({
 			orderBy: {
