03813a2241ad4e35d3d3b450d2b3157f738e6cd7
Junior <ismaeljunior1508@gmail.com>
2024-03-07 11:46:34 -0300

    vitest config

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,11 +1,12 @@
 import swc from "unplugin-swc";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	test: {
 		include: ["**/*.e2e-spec.ts"],
 		globals: true,
 		root: "./",
+		exclude: [...configDefaults.exclude, "**/data/pg/**"]
 	},
 	plugins: [
 		tsConfigPaths(),
