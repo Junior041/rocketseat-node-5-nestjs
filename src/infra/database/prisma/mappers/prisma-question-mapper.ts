53e4be5305f75afc217245c68e9f5450d4014722
Junior <ismaeljunior1508@gmail.com>
2024-03-14 17:12:00 -0300

    prisma question repository criado

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,7 @@
+import { Question as PrismaQuestion, Prisma } from "@prisma/client";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Question as PrismaQuestion } from "@prisma/client";
 
 export class PrismaQuestionMapper {
 	static toDomain({id, authorId,content,title,createdAt,slug,updatedAt, bestAnswerId}: PrismaQuestion): Question{
@@ -16,5 +16,17 @@ export class PrismaQuestionMapper {
 			slug: Slug.createFromText(slug),
 			updatedAt: updatedAt,
 		}, new UniqueEntityID(id));
+	}
+	static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+		return {
+			id: question.id.toString(),
+			authorId: question.authorId.toString(),
+			bestAnswerId: question.authorId?.toString(),
+			title: question.title,
+			content: question.content,
+			slug: question.slug.value,
+			createdAt: question.createdAt,
+			updatedAt: question.updatedAt,
+		};
 	}
 }
