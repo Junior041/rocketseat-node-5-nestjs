2f1d0fdfda85f2fa28b2e916f8be257991807cb3
Ismael <ismaeljunior1508@gmail.com>
2024-04-15 10:24:41 -0300

    delete anwer comment

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -6,9 +6,9 @@ import {
 	AnswerComment,
 	AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment';
-import { Injectable } from '@nestjs/common';
 import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment-mapper';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
 
 export function makeAnswerComment(
 	override: Partial<AnswerCommentProps> = {},
@@ -28,16 +28,18 @@ export function makeAnswerComment(
 }
 
 @Injectable()
-export class AnswerFactory{
-	constructor(private prisma: PrismaService){}
+export class AnswerCommentFactory {
+	constructor(private prisma: PrismaService) {}
 
-	async makePrismaAnswerComment(data:  Partial<AnswerCommentProps> = {}): Promise<AnswerComment>{
-		const answer = makeAnswerComment(data);
+	async makePrismaAnswerComment(
+		data: Partial<AnswerCommentProps> = {},
+	): Promise<AnswerComment> {
+		const answerComment = makeAnswerComment(data);
 
 		await this.prisma.comment.create({
-			data: 
-				PrismaAnswerCommentMapper.toPrisma(answer)
+			data: PrismaAnswerCommentMapper.toPrisma(answerComment),
 		});
-		return answer;
+
+		return answerComment;
 	}
 }
