ff23c526947e19feb3d99fc1b983bb6f0bb8ccac
Ismael <ismaeljunior1508@gmail.com>
2024-04-15 09:27:09 -0300

    edit answer

---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { BadRequestException, Body, Controller, HttpCode, Param, Put, UseGuards } from '@nestjs/common';
+import { CurrentUser } from '@/infra/auth/current-user-decorator';
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
+import { UserPayload } from '@/infra/auth/jwt.strategy';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
+import { z } from 'zod';
+import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer';
+
+const editAnswerBodySchema = z.object({
+	content: z.string(),
+});
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>;
+
+const bodyValidationType = new ZodValidationPipe(editAnswerBodySchema);
+
+@Controller('/answers/:id')
+@UseGuards(JwtAuthGuard)
+export class EditAnswerController {
+	constructor(private readonly editAnswer: EditAnswerUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+
+	async handle(
+        @Param('id') answerId: string,
+        @CurrentUser() user: UserPayload,
+        @Body(bodyValidationType) body: EditAnswerBodySchema, 
+	) {
+
+		const { content } = body;
+		const { sub } = user;
+		const result = await this.editAnswer.execute(
+			{ 
+				answerId,
+				attachmentsIds: [],
+				authorId: sub,
+				content,
+			});
+            
+		if (result.isLeft()) {
+			throw new BadRequestException();
+		}
+	}
+
+}
