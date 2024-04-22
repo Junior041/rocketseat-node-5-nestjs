08cf28b31838063306749331f56d6ab733cc7a51
Ismael <ismaeljunior1508@gmail.com>
2024-04-22 10:04:28 -0300

    comment with author

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
+import { InMemoryStudentRepository } from './in-memory-students-repository';
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-autor';
 
 export class InMemoryQuestionCommentsRepository
 implements QuestionCommentsRepository
 {
 	public items: QuestionComment[] = [];
+
+	constructor(private studentsRepository: InMemoryStudentRepository){}
 
 	async findById(id: string) {
 		const questionComment = this.items.find((item) => item.id.toString() === id);
@@ -21,6 +25,32 @@ implements QuestionCommentsRepository
 		const questionComments = this.items
 			.filter((item) => item.questionId.toString() === questionId)
 			.slice((page - 1) * 20, page * 20);
+
+		return questionComments;
+	}
+
+	async findManyByQuestionIdWithAuthor(questionId: string, { page }: PaginationParams) {
+		const questionComments = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.slice((page - 1) * 20, page * 20)
+			.map(comment => {
+
+				const author = this.studentsRepository.items.find(student => {
+					return student.id.equals(comment.authorId);
+				});
+				if (!author) {
+					throw new Error(`Author with id ${comment.authorId} not found`);
+				}
+
+				return CommentWithAuthor.create({
+					commentId: comment.id,
+					content: comment.content,
+					author: author.name,
+					authorId: author.id,
+					createdAt: comment.createdAt,
+					updatedAt: comment.updatedAt,
+				});
+			});
 
 		return questionComments;
 	}
